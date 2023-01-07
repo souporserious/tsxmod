12e1875f4807d5a1083f60c0cffb2f14d37b05ba
Travis Arnold <travis.arnold@vercel.com>
2023-01-06 22:53:23 -0800

    simplify findClosestComponentDeclaration

--- packages/utils/src/jsx/findClosestComponentDeclaration.ts
+++ packages/utils/src/jsx/findClosestComponentDeclaration.ts
@@ -1,23 +1,12 @@
 import { Node } from 'ts-morph'
 
-/** Finds the closest component declaration to a node. */
+/** Finds the closest component declaration starting from a node. */
 export function findClosestComponentDeclaration(node: Node) {
-  const componentDeclaration = node.getFirstAncestor((node) => {
+  if (Node.isFunctionDeclaration(node) || Node.isVariableDeclaration(node)) {
+    return node
+  }
+
+  return node.getFirstAncestor((node) => {
     return Node.isFunctionDeclaration(node) || Node.isVariableDeclaration(node)
   })
-
-  if (Node.isFunctionDeclaration(componentDeclaration)) {
-    return componentDeclaration
-  }
-
-  if (Node.isVariableDeclaration(componentDeclaration)) {
-    const variableDeclarationInitializer = componentDeclaration.getInitializer()
-
-    if (
-      Node.isArrowFunction(variableDeclarationInitializer) ||
-      Node.isFunctionExpression(variableDeclarationInitializer)
-    ) {
-      return componentDeclaration
-    }
-  }
 }
