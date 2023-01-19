358ceba20ebc20ef641102f86d69e80940e2bf77
Travis Arnold <travis.arnold@vercel.com>
2023-01-19 13:50:28 -0800

    allow recovery from swc transform errors

--- packages/tsxmod/src/index.ts
+++ packages/tsxmod/src/index.ts
@@ -7,25 +7,35 @@ import { transformCode } from './transform-code'
 
 const sourceFilePath = resolve(process.cwd(), process.argv[2] || '.')
 const transformFilePath = resolve(sourceFilePath, '.tsxmod/mod.ts')
+const tsConfigFilePath = resolve(sourceFilePath, 'tsconfig.json')
 const watchFlag = process.argv[3] === '--watch'
 
-const project = new Project({
-  tsConfigFilePath: resolve(sourceFilePath, 'tsconfig.json'),
-})
+console.log(
+  `Analyzing project at: ${tsConfigFilePath.replace(process.cwd(), '')}`
+)
+
+const project = new Project({ tsConfigFilePath })
 
 async function run() {
-  const transform = await readFile(transformFilePath, 'utf8')
-  const transformFn = new Function(
-    'exports',
-    'require',
-    await transformCode(transform)
-  )
-  const exports = { default: undefined }
+  console.clear()
 
-  transformFn(exports, require)
+  const transform = await readFile(transformFilePath, 'utf8')
 
   try {
-    exports.default(project)
+    const transformFn = new Function(
+      'exports',
+      'require',
+      await transformCode(transform)
+    )
+    const exports = { default: undefined }
+
+    transformFn(exports, require)
+
+    try {
+      exports.default(project)
+    } catch (error) {
+      console.error(error)
+    }
   } catch (error) {
     console.error(error)
   }
