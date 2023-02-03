8a24cd4f0b49b1de57995faa7da64e9bab237bc6
Travis Arnold <travis.arnold@vercel.com>
2023-02-03 15:56:41 -0800

    trying to fix dependency issue

--- packages/site/next.config.mjs
+++ packages/site/next.config.mjs
@@ -1,10 +1,14 @@
 import { readFile, writeFile } from 'fs/promises'
 
 export default async function () {
-  writeFile(
-    'public/tsxmod-utils.d.ts',
-    await readFile('../utils/dist/index.d.ts', 'utf8')
-  )
+  try {
+    writeFile(
+      'public/tsxmod-utils.d.ts',
+      await readFile('../utils/dist/index.d.ts', 'utf8')
+    )
+  } catch (error) {
+    console.error(error)
+  }
 
   return {
     experimental: {
