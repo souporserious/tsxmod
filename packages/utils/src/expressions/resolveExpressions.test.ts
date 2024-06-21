0c41916972e80b1e58c0ec4d57e0f404eb81b35f
Travis Arnold <ftntravis@gmail.com>
2024-06-20 23:08:25 -0700

    combine tests into one file

--- packages/utils/src/expressions/resolveExpressions.test.ts
+++ packages/utils/src/expressions/resolveExpressions.test.ts
@@ -1,5 +1,8 @@
 import { Project, SyntaxKind } from 'ts-morph'
-import { resolveLiteralExpression } from './resolveExpressions'
+import {
+  resolveLiteralExpression,
+  resolveObjectLiteralExpression,
+} from './resolveExpressions'
 
 describe('resolveLiteralExpression', () => {
   const project = new Project()
@@ -124,5 +127,49 @@ describe('resolveLiteralExpression', () => {
       .getInitializer()!
 
     expect(resolveLiteralExpression(identifier)).toBe(123)
+  })
+})
+
+describe('resolveObjectLiteralExpression', () => {
+  it('should correctly resolve property assignments', () => {
+    const project = new Project()
+    const sourceFile = project.createSourceFile(
+      'test.ts',
+      'const object = { property: "test" };'
+    )
+    const objectLiteral = sourceFile.getFirstDescendantByKind(
+      SyntaxKind.ObjectLiteralExpression
+    )
+    const object = resolveObjectLiteralExpression(objectLiteral!)
+
+    expect(object).toEqual({ property: 'test' })
+  })
+
+  it('should correctly resolve spread assignments', () => {
+    const project = new Project()
+    const sourceFile = project.createSourceFile(
+      'test.ts',
+      'const spread = { spread: "test" };\nconst object = { ...spread };'
+    )
+    const objectLiteral = sourceFile.getFirstDescendantByKind(
+      SyntaxKind.ObjectLiteralExpression
+    )
+    const object = resolveObjectLiteralExpression(objectLiteral!)
+
+    expect(object).toEqual({ spread: 'test' })
+  })
+
+  it('should correctly resolve spread assignments without identifier', () => {
+    const project = new Project()
+    const sourceFile = project.createSourceFile(
+      'test.ts',
+      'const object = { ...{ spread: "test" } };'
+    )
+    const objectLiteral = sourceFile.getFirstDescendantByKind(
+      SyntaxKind.ObjectLiteralExpression
+    )
+    const object = resolveObjectLiteralExpression(objectLiteral!)
+
+    expect(object).toEqual({ spread: 'test' })
   })
 })
